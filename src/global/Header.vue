2fc0c552747a713ed0ef573926f9aa26117c5687
NantoYuma <naitoyuma@outlook.jp>
2022-04-30 17:06:41 +0900

    routingの修正

--- src/global/Header.vue
+++ src/global/Header.vue
@@ -2,7 +2,7 @@
 	<div>
 		<v-app-bar app color="primary" dark>
 			<v-spacer></v-spacer>
-			<v-btn color="error" @click="deleteLocalStorage">削除する</v-btn>
+			<v-btn color="error" @click="deleteLocalStorage">リストを削除する</v-btn>
 		</v-app-bar>
 	</div>
 </template>
